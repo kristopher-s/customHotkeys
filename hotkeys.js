f904888df7bb37c90ac4ed4abfad954be7a04d57
Kris <34043455+krizzle100@users.noreply.github.com>
2020-04-21 21:27:29 -0400

    Update hotkeys.js
    v2

--- hotkeys.js
+++ hotkeys.js
@@ -1,11 +1,25 @@
-document.onkeydown = function hotkeys(event) {
-    let key = event.which || event.keyCode;
+// Determine Page Type
+// Prevents hotkeys from returning error on non-ticket pages.
+document.onkeydown = function detectPageType(event) {
+    key = event.which || event.keyCode;
+    let pageURI = document.documentURI;
+    if (pageURI.search('ticket/') != -1) {
+        console.log("this is a ticket page")
+        detectHotkeys(event, key);
+    } else {
+        console.log("this is not a ticket page")
+    }
+  }
+
+// Detect Hotkeys and Perform Action
+// Additional hotkeys specified here via additional else if statements
+function detectHotkeys(event, key) {
     if(event.ctrlKey && event.shiftKey && key == 76){
         const addLabelBtn = document.querySelector("#page-container > div > section > div > header > section.meta-data-container > div.labels-area > button");
         addLabelBtn.click();
     } else if (event.ctrlKey && event.shiftKey && key == 77){
         const openMacros = document.querySelector("#page-container > div > section > footer > div > div > div.macros-container.button-container > button");
         openMacros.click();
-        return;
     }
-  }
+
+}
